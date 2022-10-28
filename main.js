d69dfbfa5987e0efc9c96d233b2061a0d5b70761
Craig Weller <crg.weller@gmail.com>
2022-10-28 09:18:15 -0600

    Update with working CSS and HTML, no JS yet

---
+++ main.js
@@ -0,0 +1,19 @@
+var side = document.querySelector('#side');
+var main = document.querySelector('#main');
+var dessert = document.querySelector('#dessert');
+var entire = document.querySelector('#entire');
+
+var sideDishes = ["broccoli", "roasted potatoes" "soup"]
+var mainDishes = ["steak", "roasted squirrel", "shakshuka"]
+var dessert = ["ice cream", "gelato", "sorbet"]
+
+// window.addEventListener('load', pageLoad);
+side.addEventListener('click', showSide);
+main.addEventListener('click', showMain);
+dessert.addEventListener('click', showDessert);
+entire.addEventListener('click', showEntire);
+
+
+
+
+
